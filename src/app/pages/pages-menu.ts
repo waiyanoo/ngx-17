3e9402e78db0374eee1f79676eb9c8aabfa907ed
Wai Yan <waiyanoo.mgk@gmail.com>
2024-06-27 13:09:32 +0800

    update and fix npm issues

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -198,20 +198,20 @@ export const MENU_ITEMS: NbMenuItem[] = [
       },
     ],
   },
-  {
-    title: 'Tables & Data',
-    icon: 'grid-outline',
-    children: [
-      {
-        title: 'Smart Table',
-        link: '/pages/tables/smart-table',
-      },
-      {
-        title: 'Tree Grid',
-        link: '/pages/tables/tree-grid',
-      },
-    ],
-  },
+  // {
+  //   title: 'Tables & Data',
+  //   icon: 'grid-outline',
+  //   children: [
+  //     {
+  //       title: 'Smart Table',
+  //       link: '/pages/tables/smart-table',
+  //     },
+  //     {
+  //       title: 'Tree Grid',
+  //       link: '/pages/tables/tree-grid',
+  //     },
+  //   ],
+  // },
   {
     title: 'Miscellaneous',
     icon: 'shuffle-2-outline',
